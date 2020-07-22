0c9bb905be91afb4daa8b13beabb07b7aacc97fe
a <seichiclick081@gmail.com>
2020-07-22 21:40:59 +0900

    ver 1.0

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-\cp -r ../sorce 
+echo "Please input the file name:" 
+
+#キーボード入力待ち
+read name
+
+#フォルダ生成
+\cp -r ./Sorce ./${name}`date "+%Y%m%d_%H%M"`
 
 #ホワイトリストコピー
-\cp -f ../akari/whitelist.json ./whitelist.json
+\cp -f ./akari/whitelist.json ./whitelist.json
